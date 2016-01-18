31309bdfdc225bb4469eb378af101500f181ee60
@dantaylorseo <dan@tailored.im>
2016-01-18 23:49:29 +0000

    initial commit

---
+++ EvertonNewsApp/Stories.swift
@@ -0,0 +1,24 @@
+//
+//  Stories.swift
+//  EvertonNewsApp
+//
+//  Created by Dan Taylor on 08/01/2016.
+//  Copyright © 2016 Dan Taylor. All rights reserved.
+//
+
+import UIKit
+
+class Stories: NSObject {
+    
+    var title: String!
+    var date: NSDate!
+    var author: String!
+    var site: String!
+    var category: String!
+    var link: String!
+    var content: String!
+    var desc: String!
+    var image: String?
+    var thumb: String?
+    
+}
